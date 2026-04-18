6e4ec4760cdec2422c8f5f3a08350f0e4d6a43f0
Ryan Wong <me@ryanwong.uk>
2026-04-18 20:00:40 +0100

    test(e2e): resetAndSeed baseline via /testdata

---
+++ tests/e2e/setup/baseline.ts
@@ -0,0 +1,39 @@
+import { getE2EContext } from './bootstrap.js';
+import { fakeChatInput } from './synthesizer.js';
+import { wipeTestDb } from './db.js';
+import { initDatabase } from '../../../src/database/db.js';
+import testdataCmd from '../../../src/commands/testdata.js';
+import type { ChatInputCommandInteraction, TextBasedChannel } from 'discord.js';
+
+export async function resetAndSeed(): Promise<void> {
+  const { client, guild, officer } = getE2EContext();
+
+  wipeTestDb();
+  initDatabase();
+
+  const channel = guild.systemChannel ?? guild.channels.cache.find((c) => c.isTextBased())!;
+
+  const reset = fakeChatInput({
+    client,
+    guild,
+    channel: channel as TextBasedChannel,
+    member: officer,
+    user: officer.user,
+    commandName: 'testdata',
+    subcommand: 'reset',
+    options: { confirm: true },
+  });
+  await testdataCmd.execute(reset as unknown as ChatInputCommandInteraction);
+
+  const seedAll = fakeChatInput({
+    client,
+    guild,
+    channel: channel as TextBasedChannel,
+    member: officer,
+    user: officer.user,
+    commandName: 'testdata',
+    subcommand: 'seed_all',
+    options: { discord: true },
+  });
+  await testdataCmd.execute(seedAll as unknown as ChatInputCommandInteraction);
+}
